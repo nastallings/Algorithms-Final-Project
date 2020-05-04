0d8d636e863959940954677ca743c3f0ae56690a
nstal <nstal@DESKTOP-LMPKSII>
2020-05-04 14:50:01 -0400

    Includes the time to complete tasks

--- Algorithms Final Project D2020/src/Hashing/Hashing_Test.java
+++ Algorithms Final Project D2020/src/Hashing/Hashing_Test.java
@@ -3,7 +3,6 @@ package Hashing;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.LinearProbingHashST;
 
-
 public class Hashing_Test {
 
 	public static void main(String[] args) {
@@ -22,26 +21,22 @@ public class Hashing_Test {
   
         
         //Run the test 4 times for the 4 different Dictionary sizes
-        for(int i=1; i<=4; i++) {
+        for(int i=0; i<4; i++) {
             LinearProbingHashST<String, Integer> st = new LinearProbingHashST<String, Integer>();
         	long start = System.currentTimeMillis();
-            System.out.println(start);
-
-        	for(int j=0; j<English_Dict_Array.length/i; j++) {
+        	for(int j=0; j<(int)(English_Dict_Array.length/(Math.pow(2,i))); j++) {
         		st.put(English_Dict_Array[j], 1);
         	}
         	long finish= System.currentTimeMillis();
-            System.out.println(finish);
-
-        	long TimeToCreate= finish-start;
+        	long TimeToCreate= (finish-start);
         	timeToPut[i]=TimeToCreate;
    
-        	start = System.currentTimeMillis();
-        	for(int j=0; j<English_Dict_Array.length/i; j++) {
+        	long startGet = System.currentTimeMillis();
+        	for(int j=0; j<(int)(English_Dict_Array.length/(Math.pow(2,i))); j++) {
         		st.get(English_Dict_Array[j]);
         	}
-        	finish= System.currentTimeMillis();        
-        	long TimeToGet=finish-start;
+        	long finishGet= System.currentTimeMillis(); 
+        	long TimeToGet=finishGet-startGet;
         	timeToGet[i]=TimeToGet;
         }
         
@@ -50,7 +45,7 @@ public class Hashing_Test {
         long[] timeToGetStatic = new long[6];
         
         for(int i = 0; i< mf.length; i++) {
-            LinearProbingHashST<String, Integer> StaticHash = new LinearProbingHashST<String, Integer>((int)mf[i]*321165);
+            LinearProbingHashST<String, Integer> StaticHash = new LinearProbingHashST<String, Integer>((int)(mf[i]*321165));
             long start = System.currentTimeMillis();
         	for(int j=0; j<English_Dict_Array.length; j++) {
         		StaticHash.put(English_Dict_Array[j], 1);
@@ -68,7 +63,22 @@ public class Hashing_Test {
         	timeToGetStatic[i]=TimeToGet;
         }
        
-              
+        
+		System.out.println("N	 Time to Put(ms)		Time to Get(ms)");
+         for(int i=0; i<4; i++) {
+      		System.out.print(String.format("%4d,", (int)(English_Dict_Array.length/(Math.pow(2,i)))));
+     		System.out.print(String.format("%8d,", timeToPut[i]));
+    		System.out.print(String.format("%26d,", timeToGet[i]));
+    		System.out.println();
+         }
+         
+         System.out.println("Size	 Time to Put Constant Size(ms)		Time to Get Constant Size(ms)");
+         for(int i=0; i<6; i++) {
+      		System.out.print(String.format("%4d,", (int)(mf[i]*321165)));
+     		System.out.print(String.format("%16d,", timeToPutStatic[i]));
+    		System.out.print(String.format("%48d,", timeToGetStatic[i]));
+    		System.out.println();
+         }
         
 	}
 
