caa3a9ac80a1bdc349b30fd42e225dd6ef913717
nstal <nstal@DESKTOP-LMPKSII>
2020-05-04 09:01:38 -0400

    "Added the english dictionary file"

--- Algorithms Final Project D2020/src/Hashing/Hashing_Test.java
+++ Algorithms Final Project D2020/src/Hashing/Hashing_Test.java
@@ -1,9 +1,8 @@
 package Hashing;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.LinearProbingHashST;
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.io.File; // Import the File class
-import java.util.Scanner; // Import the Scanner class to read text files
+
 
 public class Hashing_Test {
 
@@ -15,18 +14,13 @@ public class Hashing_Test {
         //Create the file object, the Scanner, and an array to hold words
         String[] English_Dict_Array= new String[321165];
 
-        try {
-        File myDict = new File("words.english.txt");
-        Scanner myReader = new Scanner(myDict);
+        In myDict = new In("words.english.txt");
         int index = 0;
         //Fill the English_Dict_Array and close the reader
-        while(myReader.hasNextLine()) {
-        	English_Dict_Array[index++] = myReader.nextLine();
+        while(myDict.hasNextLine()) {
+        	English_Dict_Array[index++] = myDict.readLine();
         }
-        myReader.close();
-       }catch(FileNotFoundException e) {
-    	   System.out.println("The desired file was not found.");
-       }
+  
         
         //Run the test 4 times for the 4 different Dictionary sizes
         for(int i=1; i<=4; i++) {
@@ -47,14 +41,31 @@ public class Hashing_Test {
         	timeToGet[i]=TimeToGet;
         }
         
-        LinearProbingHashST<String, Integer> st_eight = new LinearProbingHashST<String, Integer>(8*321165);
-        LinearProbingHashST<String, Integer> st_four = new LinearProbingHashST<String, Integer>(4*321165);
-        LinearProbingHashST<String, Integer> st_two = new LinearProbingHashST<String, Integer>(2*321165);
-        LinearProbingHashST<String, Integer> st_one_half = new LinearProbingHashST<String, Integer>((int)1.5*321165);
-        LinearProbingHashST<String, Integer> st_one_quarter = new LinearProbingHashST<String, Integer>((int)1.25*321165);
-        LinearProbingHashST<String, Integer> st_one_eigth = new LinearProbingHashST<String, Integer>((int)1.125*321165);
-
+        double[] mf = {8,4,2,1.5, 1.25, 1.125};
+        long[] timeToPutStatic = new long[6];
+        long[] timeToGetStatic = new long[6];
         
+        for(int i = 0; i< mf.length; i++) {
+            LinearProbingHashST<String, Integer> StaticHash = new LinearProbingHashST<String, Integer>((int)mf[i]*321165);
+            long start = System.currentTimeMillis();
+        	for(int j=0; j<English_Dict_Array.length; j++) {
+        		st.put(English_Dict_Array[j], 1);
+        	}
+        	long finish= System.currentTimeMillis();
+        	long TimeToCreate= finish-start;
+        	timeToPutStatic[i]=TimeToCreate;
+   
+        	start = System.currentTimeMillis();
+        	for(int j=0; j<English_Dict_Array.length; j++) {
+        		st.get(English_Dict_Array[j]);
+        	}
+        	finish= System.currentTimeMillis();        
+        	long TimeToGet=finish-start;
+        	timeToGetStatic[i]=TimeToGet;
+        }
+       
+        
+      System.out.println(English_Dict_Array.length);
         
         
 	}
